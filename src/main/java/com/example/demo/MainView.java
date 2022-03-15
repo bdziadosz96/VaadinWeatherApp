0decf9d532fcb8ecbf048fd2ddcd15a896d49d1f
bdziadosz96 <bdziadosz96@icloud.com>
2022-03-15 21:56:07 +0100

    Add logo display

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -1,16 +1,17 @@
 package com.example.demo;
 
-import com.example.demo.weather.Sys;
 import com.example.demo.weather.Weather;
 import com.example.demo.weather.WeatherController;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import java.util.Objects;
+import com.vaadin.flow.server.StreamResource;
 
 @Route("")
 @CssImport("./styles/shared-styles.css")
@@ -22,9 +23,17 @@ public class MainView extends VerticalLayout {
     public MainView(WeatherController controller) {
         this.controller = controller;
         addClassName("centered-content");
-        addComponentAsFirst(new H1("Welcome to weather application"));
+
+        add(new H2("Welcome to weather application"));
+        StreamResource imageResource = new StreamResource("myimage.png",
+                () -> getClass().getResourceAsStream("/images/myimage.png"));
+        Image image = new Image(imageResource, "My Streamed Image");
+
+        image.setWidth("75%");
+        image.setHeight("75%");
 
         HorizontalLayout center = new HorizontalLayout();
+        center.setAlignItems(Alignment.CENTER);
         TextField cityName = new TextField("Type your city");
         center.add(cityName);
 
@@ -37,6 +46,6 @@ public class MainView extends VerticalLayout {
         }
 
 
-        add(center);
+        add(image, center);
     }
 }
