fa337610fd06dce9513a5da01c8b344aa1d6c6c6
bdziadosz96 <bdziadosz96@icloud.com>
2022-03-29 22:58:09 +0100

    New email distribution

--- src/main/java/com/example/demo/email/service/EmailService.java
+++ src/main/java/com/example/demo/email/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.example.demo.email.service;
 
-import org.springframework.stereotype.Service;
-
 public interface EmailService {
     void sendEmail(String toEmail,
                    String body);
